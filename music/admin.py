af9aea82031a6eb6a4e56bb49317e42b5d25503d
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-01-01 19:53:17 -0700

    refactor metadata relationships and song contribution

--- music/admin.py
+++ music/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Song, Playlist
+from .models import Song, Playlist, Entity, EntityAlias, Album
 
+admin.site.register(Album)
+admin.site.register(Entity)
+admin.site.register(EntityAlias)
 admin.site.register(Song)
 admin.site.register(Playlist)
--- music/urls.py
+++ music/urls.py
@@ -5,8 +5,9 @@ app_name = 'music'
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
-    path('<str:spotify_uri>/contribute/', views.contribute, name='contribute'),
+    path('contribute-song/', views.contribute_song, name='contribute_song'),
+    path('spot-song/<str:spotify_uri>/', views.contribute_spotify_song, name='contribute_spotify_song'),
     path('contributespot/', views.contribute_from_spotify, name='contributespot'),
     path('<int:id>/edit/', views.edit_song, name='edit_song'),
-    path('hornists/', views.hornists, name = 'hornists'),
+    path('artists/', views.artists, name = 'artists'),
 ]
