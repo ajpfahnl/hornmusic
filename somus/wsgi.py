cd9d207698723ab6da8950c2760aff339819d654
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-01-02 20:18:10 -0700

    Revert "Revert "edit django settings and rename files for name change""
    This reverts commit e0eb726073e838e9772dc1258dd7a0007b9d2332.

--- somus/wsgi.py
+++ somus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for metronomic project.
+WSGI config for somus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metronomic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'somus.settings')
 
 application = get_wsgi_application()
